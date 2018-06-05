0f4b1c5c4c3ac9982efb986c9b1c8c45e102b2e0
Bishwa Poudel <bispoul181@gmail.com>
2018-06-05 10:02:28 +0545

    Add PATCH /todo/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const {ObjectID} = require('mongodb')
+const _ = require('lodash')
 
 const {mongoose} = require('./../db/mongo-connect')
 const {Todo} = require('./../db/collections/todo')
@@ -72,6 +73,44 @@ app.get('/todo/:id', (req, res) => {
         })
     })
 })
+
+/**
+ * PATCH /todo/:id
+ * update todo
+ */
+app.patch('/todo/:id', (req, res) => {
+    // check if id is valid or not
+    const id = req.params.id
+    if(!ObjectID.isValid(id)){
+        res.status(404).send({
+            message: 'ID doesn\'t exist'
+        })
+    }
+
+    // construct new data 
+    const newData = _.pick(req.body, ['text', 'completed'])
+    if(_.isBoolean(newData.completed) && newData.completed){
+        newData.completedAt = new Date().getTime()
+    }else{
+        newData.completedAt = null
+    }
+    
+    // update data
+    Todo.findByIdAndUpdate(id, {$set: newData}, {new: true}).then(data => {
+        if(data){
+            res.status(200).send({data})
+        }else{
+            res.status(404).send({
+                message: 'ID doesn\'t exist'
+            })
+        }
+    }).catch(err => {
+        res.status(400).send({
+            message: 'An error occured. Try Again'
+        })
+    })
+})
+
 app.listen(3000, () => {
     console.log('Connection established on port 3000')
 })
