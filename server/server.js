2b24929df82711bb65027c0f3553b5cd7f13e488
Bishwa Poudel <bispoul181@gmail.com>
2018-06-05 06:54:39 +0545

    add GET /todos

--- server/server.js
+++ server/server.js
@@ -5,17 +5,36 @@ const {Todo} = require('./../db/collections/todo')
 
 const app = express()
 
-const newTodo = new Todo({
-    text: 'Hello',
-    completed: true
+/**
+ * GET /todos
+ * get all todos
+ */
+app.get('/todos', (req, res) => {
+    Todo.find().then(data => {
+        if(data.length > 0) res.status(200).send({data})
+        else {
+            res.status(200).send({
+                message: 'No todos available'
+            })
+        }
+    }).catch(err => {
+        res.status(400).send({
+            message: 'An error occured. Try Again'
+        })
+    })
 })
 
-newTodo.save().then((doc) => {
-    console.log('Saved todo successfully')
-    console.log(doc)
-}).catch((e) => {
-    console.log('Unable to save todo')
-})
+// const newTodo = new Todo({
+//     text: 'Hello',
+//     completed: true
+// })
+
+// newTodo.save().then((doc) => {
+//     console.log('Saved todo successfully')
+//     console.log(doc)
+// }).catch((e) => {
+//     console.log('Unable to save todo')
+// })
 
 app.listen(3000, () => {
     console.log('Connection established on port 3000')
